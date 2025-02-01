8f8e9721e8d14815004e54d3e8ba0089542e5536
Hitman-97 <150859674+Hitman-97@users.noreply.github.com>
2025-02-01 14:37:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './App.css';  // Optional: Add your own styles
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
